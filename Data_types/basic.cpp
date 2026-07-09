bff491f1471d158abe1e5ee37d2598bc02888a45
Mahbub <mahbub16575@gmail.com>
2026-07-09 19:45:44 +0530

    basic data type added

---
+++ Data_types/basic.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout<<sizeof(int)<<" BYTES"<<endl;
+    cout<<sizeof(long int)<<" BYTES"<<endl;
+    cout<<sizeof(short int )<<" BYTES"<< endl;
+    cout << sizeof(long long int )<<" BYTES"<<endl;
+    return 0;
+}
